f59ddd80015df8987e585b39f9a8ca4bb771a18d
chues5124 <s1061641@gm.pu.edu.tw>
2019-01-17 17:46:26 +0800

    finalgame

---
+++ Assets/Gamef.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamef : MonoBehaviour {
+    public GameObject Emeny;
+    public float time;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time > 0.5f)
+        {
+            Vector3 pos = new Vector3(Random.Range(-2.5f, 2.5f), 4.5f, 0);
+            Instantiate(Emeny, pos, transform.rotation);
+            time = 0f;
+        }
+
+    }
+}
--- Assets/enemyc.cs
+++ Assets/enemyc.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulcontorl : MonoBehaviour {
+public class enemyc : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,11 +11,11 @@ public class bulcontorl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.position += new Vector3(0, 0.1f, 0);
+        gameObject.transform.position += new Vector3(0, -0.01f, 0);
     }
     void OnTriggerEnter2D(Collider2D col) //名為col的觸發事件
     {
-        if (col.tag == "ship" || col.tag == "Bullet") //如果碰撞的標籤是Ship或Bullet
+        if (col.tag == " ship" || col.tag == "Bullet") //如果碰撞的標籤是Ship或Bullet
         {
             Destroy(col.gameObject); //消滅碰撞的物件
             Destroy(gameObject); //消滅物件本身
